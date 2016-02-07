9255fec90266d361eb1ef08cfb60364cad90d3f2
Simon Lepel <simbo@simbo.de>
2016-02-07 18:32:26 +0100

    add test for not bundling on errors

---
+++ test/fixtures/baz.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var fail = require('does-not-exist');
--- test/index.js
+++ test/index.js
@@ -11,7 +11,7 @@ describe('gulp-watchify-browserify', function() {
 
   it('should create separate bundles by globbing entries', function(done) {
     var streamsCount = 0;
-    gWB('./test/fixtures/*.js', {
+    gWB('./test/fixtures/@(foo|bar).js', {
       watch: false,
       browserify: {paths: ['./test/fixtures/modules']}
     }, function(stream) {
@@ -29,7 +29,7 @@ describe('gulp-watchify-browserify', function() {
 
   it('should watch and rebundle', function(done) {
     var streamsCount = 0;
-    gWB('./test/fixtures/*.js', {
+    gWB('./test/fixtures/@(foo|bar).js', {
       watch: true,
       browserify: {paths: ['./test/fixtures/modules']}
     }, function(stream) {
@@ -42,7 +42,7 @@ describe('gulp-watchify-browserify', function() {
     }, function() {
     });
     setTimeout(function() {
-      touch('./test/fixtures/modules/hello.js', false, function() {
+      touch('./test/fixtures/modules/hello.js', {}, function() {
         setTimeout(function() {
           assert.equal(streamsCount, 4);
           done();
@@ -52,9 +52,23 @@ describe('gulp-watchify-browserify', function() {
   });
 
   it('should do nothing if no files found', function(done) {
-    gWB('./does/not/exist/**/*', {}, function() {
+    gWB('./does/not/exist/**/*', null, function() {
       throw new Error('FAILED');
     }, function() {
+      done();
+    });
+  });
+
+  it('should not bundle on errors', function(done) {
+    var streamsCount = 0;
+    gWB('./test/fixtures/baz.js', null, function(stream) {
+      return stream
+        .pipe(streamAssert.first(function() {
+          streamsCount++;
+        }))
+        .pipe(streamAssert.end());
+    }, function() {
+      assert.equal(streamsCount, 0);
       done();
     });
   });
